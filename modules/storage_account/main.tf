5997beee26c2ca9c8c9cc859470be172735e0e23
Nandu-S47 <snarendar789@gmail.com>
2025-03-13 08:26:28 +0530

    Module changes

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -2,7 +2,7 @@
 resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
   resource_group_name      = modules.resource_group.output.name
-  location                 = amodules.resource_group.output.location
+  location                 = modules.resource_group.output.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
