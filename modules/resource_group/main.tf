369c849fc7914a5aa10c0b47c28124bac0f295f9
Nandu-S47 <snarendar789@gmail.com>
2025-03-13 08:26:28 +0530

    Module changes

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -6,7 +6,6 @@ resource "azurerm_resource_group" "rg" {
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
-
 output "location" {
   value = azurerm_resource_group.rg.location
 }
