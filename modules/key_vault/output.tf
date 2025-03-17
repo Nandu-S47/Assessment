ff8ff74fc1228dd9d5e1194b33345ddc2fe0d212
Nandu-S47 <snarendar789@gmail.com>
2025-03-17 12:21:24 +0000

    private endpoint added to AKV

---
+++ modules/key_vault/output.tf
@@ -0,0 +1,9 @@
+output "key_vault_id" {
+  value = azurerm_key_vault.example.id
+  
+}
+
+output "vault_uri" {
+  value = azurerm_key_vault.example.vault_uri
+  
+}
--- modules/key_vault/variables.tf
+++ modules/key_vault/variables.tf
@@ -15,3 +15,39 @@ variable "akv_rg_name" {
   type        = string
   
 }
+
+variable "private_endpoint_name" {
+  description = "The name of the private endpoint"
+  type        = string
+  
+}
+
+variable "akv_subnet1_id" {
+  description = "The ID of the subnet in which the private endpoint will be created"
+  type        = string
+  
+}
+
+variable "private_service_connection_name" {
+  description = "The name of the private service connection"
+  type        = string  
+  
+}
+
+variable "private_dns_zone_group_name" {
+  description = "The name of the private DNS zone group"
+  type        = string
+  
+}
+
+variable "prvdns_vnet_link_name" {
+  description = "The name of the private DNS zone virtual network link"
+  type        = string
+  
+}
+
+variable "vnet_id_to_link" {
+  description = "The ID of the virtual network to link to the private DNS zone"
+  type        = string
+  
+}
